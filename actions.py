4c4369a0c3a95e46e01d9a96faeed018060fb2dd
Nizam Omari <nizam_omari@hotmail.com>
2019-08-04 20:37:55 -0300

    dunmop

--- actions.py
+++ actions.py
@@ -26,7 +26,8 @@ class Explicacao(Action):
 
         tema = tracker.get_slot('tema_para_ser_explicado')
         print('chega aqui =D')
-        pprint(dumps(tracker))
+        pprint(tracker.current_slot_values())
+        pprint(tracker.slots)
         dispatcher.utter_message("select * from restaurants where cuisine='{0}' limit 1".format(tema))
 
         return []
