1d5b9642707779682265a9bd8c6e2463e208f4da
Nizam Omari <nizam_omari@hotmail.com>
2019-08-17 22:43:00 -0300

    aa

--- actions.py
+++ actions.py
@@ -25,10 +25,18 @@ class Explicacao(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         # tema = tracker.get_slot('tema_para_ser_explicado')
-        print('chega aqui =D')
+        # print('chega aqui =D')
 
-        pprint(next(tracker.get_latest_entity_values('tema_para_ser_explicado')))
-        pprint(dumps(tracker.latest_action_name))
-        dispatcher.utter_message("select * from restaurants where cuisine='{0}' limit 1".format('ok ok'))
+        dispatcher.utter_message(
+            "Ok, vou pesquisar mais sobre {0}".format(next(tracker.get_latest_entity_values('tema_para_ser_explicado')))
+        )
+
+        dispatcher.utter_message(
+            " Agora fazer alguma busca sobre {0} ".format(next(tracker.get_latest_entity_values('tema_para_ser_explicado')))
+        )
+
+        dispatcher.utter_message(
+            "Conseguiu entender ? "
+        )
 
         return []
