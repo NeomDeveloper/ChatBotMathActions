108170bc9f764f21deacfaa6a2fa31b503cf69ee
Nizam Omari <nizam_omari@hotmail.com>
2019-08-24 18:32:28 -0300

    ajustes

--- actions.py
+++ actions.py
@@ -11,8 +11,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from pprint import pprint
-from json import dumps
+from rasa_sdk.events import SlotSet, UserUtteranceReverted, ConversationPaused
 
 
 class Explicacao(Action):
@@ -20,26 +19,34 @@ class Explicacao(Action):
     def name(self) -> Text:
         return "explicando_algo"
 
+    @staticmethod
+    def required_slots(tracker):
+        return ["tema_para_ser_explicado"]
+
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # tema = tracker.get_slot('tema_para_ser_explicado')
+        tema = next(tracker.get_latest_entity_values('tema_para_ser_explicado'))
 
-        print('chega aqui 1')
-        tema = next(tracker.get_latest_entity_values('tema_para_ser_explicado')) if next(tracker.get_latest_entity_values('tema_para_ser_explicado')) else 'Nada'
-        print('chega aqui 2' + tema)
+        print(tema)
 
-        dispatcher.utter_message(
-            "Ok, vou pesquisar mais sobre {0}".format(tema)
-        )
+        # if tema not in Explicacao.required_slots(tracker):
+        #     dispatcher.utter_message(
+        #         "Desculpe, parece ainda não sei sobre esse tema =/"
+        #     )
+        #     return []
 
-        dispatcher.utter_message(
-            " Agora fazer alguma busca sobre {0} ".format(tema)
-        )
+        dispatcher.utter_message("Ok, pesquisando sobre {0}... ".format(tema))
 
-        dispatcher.utter_message(
-            "Conseguiu entender ? "
-        )
+        dispatcher.utter_message(self.explicar(tema))
 
-        # dispatcher.utter_button_message('entendeu?', List[domain])
+        dispatcher.utter_message("Conseguiu entender?")
+        if not tema.strip():
+            # Só colocar o slot se o tema não estiver vazio =D
+            return [SlotSet('tema_para_ser_explicado', tema)]
 
         return []
+
+    def explicar(self, tema):
+        return 'Aqui a função para pesquisar sobre: ' + tema
